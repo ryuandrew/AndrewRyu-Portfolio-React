9f16ea153a2c86f6534d38b8ad8ec3ac317affb8
Andrew Ryu <andrewryu@outlook.com>
2022-06-11 22:19:05 -0700

    updated experience component

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,8 +1,79 @@
 import React from "react";
 import "./experience.css";
+import { AiFillCheckCircle } from "react-icons/ai";
 
 const Experience = () => {
-  return <section id="experience">Experience</section>;
+  return (
+    <section id="experience">
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+        {/* frontend */}
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <AiFillCheckCircle className="experience__details-icon" />
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light"></small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <AiFillCheckCircle className="experience__details-icon" />
+              <div>
+                <h4>CSS</h4>
+                <small className="text-light"></small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <AiFillCheckCircle className="experience__details-icon" />
+              <div>
+                <h4>JavaScript</h4>
+                <small className="text-light"></small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <AiFillCheckCircle className="experience__details-icon" />
+              <div>
+                <h4>Bootstrap</h4>
+                <small className="text-light"></small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <AiFillCheckCircle className="experience__details-icon" />
+              <div>
+                <h4>React</h4>
+                <small className="text-light"></small>
+              </div>
+            </article>
+          </div>
+        </div>
+        {/* backend */}
+        <div className="experience__backend">
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <AiFillCheckCircle className="experience__details-icon" />
+              <h4>Node JS</h4>
+              <small className="text-light"></small>
+            </article>
+            <article className="experience__details">
+              <AiFillCheckCircle className="experience__details-icon" />
+              <h4>MySQL</h4>
+              <small className="text-light"></small>
+            </article>
+            <article className="experience__details">
+              <AiFillCheckCircle className="experience__details-icon" />
+              <h4>MongoDB</h4>
+              <small className="text-light"></small>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Experience;
