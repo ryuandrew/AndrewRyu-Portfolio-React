1cded00b1b6bc82aa64251302f5633551927a144
Andrew Ryu <andrewryu@outlook.com>
2022-08-01 17:00:09 -0700

    updated resume

--- src/components/header/RESUME.jsx
+++ src/components/header/RESUME.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import AR_RESUME from "../../assets/ar_resume.pdf";
+import AR_RESUME from "../../assets/AndrewRyu-Resume-SW-Engineer.pdf";
 
 const RESUME = () => {
-  return (
-    <div className="resume">
-      <a href={AR_RESUME} download className="btn">
-        Download Resume
-      </a>
-      <a href="#contact" className="btn">
-        Contact me
-      </a>
-    </div>
-  );
+    return (
+        <div className="resume">
+            <a href={AR_RESUME} download className="btn">
+                Download Resume
+            </a>
+            <a href="#contact" className="btn">
+                Contact me
+            </a>
+        </div>
+    );
 };
 
 export default RESUME;
