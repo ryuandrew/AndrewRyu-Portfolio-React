866a80e566c05e48cacf29fe759b150e64885675
Andrew Ryu <andrewryu@outlook.com>
2022-08-03 13:09:23 -0700

    added a form to send email

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./footer.css";
 
 const Footer = () => {
-  return <div id="footer1"> Footer </div>;
+    return <div id="footer1"> </div>;
 };
 
 export default Footer;
